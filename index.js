6e341339b67f11d8b5ff74ccb7f78b7a3a1b1f01
Chantal Justamond <cjustamm66@me.com>
2017-10-27 22:11:18 -0600

    api/current_user showing right info :tada:

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys');
+require('./models/user');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/calback'
-  }, (accessToken) => {
-    con.log(accessToken);
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, //maxAge = 30 days. passed in in miliseconds.
+    keys: [keys.cookieKey]
   })
 );
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
