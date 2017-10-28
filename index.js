eb4cf9ed2b9d82373a34a8b8373249fab39c5db9
Chantal Justamond <cjustamm66@me.com>
2017-10-27 18:32:58 -0600

    ignored file added

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/calback'
+  }, (accessToken) => {
+    con.log(accessToken);
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 
