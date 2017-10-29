2d56ca80b2123494b6bd244eb437742e12d63f6e
Chantal Justamond <cjustamm66@me.com>
2017-10-29 12:22:55 -0600

    button added 'Add credits' with just some materialize class

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -5,10 +5,14 @@ class Payments extends Component {
   render() {
     return (
       <StripeCheckout
+        name="Mighty Mail"
+        description="$5 for 5 email credits"
         amount={500} // amount goes by cents 500 cents = 5 dollars
         token={token => console.log(token)} // sent by stripe
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+        <button className="btn">Add Credits</button>
+      </StripeCheckout>
     );
   }
 }
