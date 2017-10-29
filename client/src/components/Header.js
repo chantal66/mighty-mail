0dd5b19df578f174eddc1cb739a9177e35d08bfe
Chantal Justamond <cjustamm66@me.com>
2017-10-28 23:09:34 -0600

    user can login and logout with google oauth. 😜

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <li><a href="/auth/google">Login With Google</a></li>;
+      default:
+        return <li><a href="/api/logout">Logout</a></li>;
+    }
+  }
+
   render() {
     return (
       <nav>
@@ -8,15 +20,15 @@ class Header extends Component {
           <a href="" className="left brand-logo">
             Mighty Mail
           </a>
-          <ul className="right">
-            <li>
-              <a href="">Login with Google</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
