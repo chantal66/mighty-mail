a26da77cf0da2e735b5e177d0627175f03cd76c7
Chantal Justamond <cjustamm66@me.com>
2017-10-29 14:00:44 -0600

    /api/stripe added with specific information for adding credis to user

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,14 +1,19 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
-  app.post('/api/stripe', async (req, res) => {
+  app.post('/api/stripe', requireLogin, async (req, res) => {
     const charge = await stripe.charges.create({
       amount: 500, // 500 cents = 5 dollars
       currency: 'usd',
       description: '$5 dollars for 5 credits',
       source: req.body.id
     });
-    console.log(charge);
+
+    req.user.credits += 5;
+    const user = await req.user.save(); // req.user is the user model.
+
+    res.send(user);
   });
 };
