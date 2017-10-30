9070763179e93fc17166e3b0b8ad9fe2a4d62da5
Chantal Justamond <cjustamm66@me.com>
2017-10-30 16:14:27 -0600

    Mailer contructor added.

--- services/Mailer.js
+++ services/Mailer.js
@@ -2,6 +2,16 @@ const sendgrid = require('sendgrid');
 const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
-class Mailer extends helper.Mail {}
+class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super();
+
+    // sendgrid specific setup
+    this.from_email = new helper.Email('no-reply@mightymail.com');
+    this.subject = subject;
+    this.body = new helper.Content('text/html', content);
+    this.recipients = this.formatAddresses(recipients); //specifies that im sending emails to multiple people
+  }
+}
 
 module.exports = Mailer;
