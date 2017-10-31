9918e5e47e00aaa60fd21a62f9001cd40677e5c9
Chantal Justamond <cjustamm66@me.com>
2017-10-31 11:35:33 -0600

    redux form Field helper so we can render traditional HTML

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,6 +1,6 @@
 // SurveyForm shows a form for a user to add input.
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
 
 class SurveyForm extends Component {
   render() {
