b7e72e6d336c0366a977f422d2b0cfd7b68c4acf
Chantal Justamond <cjustamm66@me.com>
2017-10-31 10:06:43 -0600

    replaced vanilla anchor tag with Link tag from react router

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
   return (
     <div>
       Dashboard
       <div className="fixed-action-btn">
-        <a className="btn-floating btn-large red" href="">
+        <Link to="/surveys/new" className="btn-floating btn-large red" href="">
           <i className="material-icons">add</i>
-        </a>
+        </Link>
       </div>
     </div>
   );
